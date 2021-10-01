85d90ecd00f2a0ac5316d5ff07b8b674692ca292
Vicachu42 <69855483+Vicachu42@users.noreply.github.com>
2021-10-01 13:21:52 +0200

    App test fix

--- meetup-app/tests/unit/App.spec.js
+++ meetup-app/tests/unit/App.spec.js
@@ -1,5 +1,16 @@
+import { shallowMount, mount } from '@vue/test-utils';
+import App from '@/App.vue';
 
-/*   it('should push event object from meetups to registeredMeetups upon clicking button', async () => { }); */
+describe('App.vue', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(App)
+  });
+
+  it('should at least have one passing test', () => { });
+
+  /*   it('should push event object from meetups to registeredMeetups upon clicking button', async () => { }); */
 
   // Lower priority
-/*   it('should push user name into array of participants', async () => { }); */
+  /*   it('should push user name into array of participants', async () => { }); */
+});
