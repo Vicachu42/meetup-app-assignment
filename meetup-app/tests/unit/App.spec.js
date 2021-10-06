3d2aa444c1367ead6dcbca720c8643a7bdfdf625
Vicachu42 <69855483+Vicachu42@users.noreply.github.com>
2021-10-06 11:47:26 +0200

    Fixed App.spec test

--- meetup-app/tests/unit/App.spec.js
+++ meetup-app/tests/unit/App.spec.js
@@ -5,11 +5,10 @@ describe('App.vue', () => {
   let wrapper, data;
   beforeEach(() => {
     data = meetups;
-    /*     wrapper = mount(App) */
+    wrapper = shallowMount(App)
   });
 
   it('should push event object from meetups to registeredMeetups upon clicking button', async () => {
-    // This is testing the method, but is it testing that the right content is pushed to the right place?
     const attendMeetup = jest.spyOn(App.methods, 'registerToMeetup');
     const wrapper = mount(App);
 
@@ -17,45 +16,10 @@ describe('App.vue', () => {
     await buttonElem.trigger('click');
 
     expect(attendMeetup).toBeCalled();
-
-    /*     const wrapper = mount(App, {
-          data() {
-            return {
-              meetups
-            }
-          }
-        }) */
-
-    /*     const meetup = [{
-          id: 1,
-          time: "FRI, DEC 24, 2021, 3:00 PM CEST",
-          title: "Extraterrestrial Meetup #2",
-          place: "66°13′12.6″N 172°19′36.9″E",
-          attendees: 5,
-        }];
-        console.log(meetup); */
-
-    /*     const expected = data[0].time
-        const wrapper = mount(App, {
-          data() {
-            return {
-              meetups: data[0]
-            }
-          }
-        })
-    
-        console.log(data);
-        const buttomElem = wrapper.find('.attend-meetup');
-        await buttomElem.trigger('click');
-     */
   });
 
   // Lower priority
   /*   it('should push user name into array of participants', async () => { }); */
-
-  /*   it('should not be able to register to an event more than once', async () => {
-
-    }) */
 });
 
 const meetups = [
@@ -64,27 +28,48 @@ const meetups = [
     time: "FRI, DEC 24, 2021, 3:00 PM CEST",
     title: "Extraterrestrial Meetup #2",
     place: "66°13′12.6″N 172°19′36.9″E",
-    attendees: 5,
+    attendees: [
+      "Zoidberg",
+      "Boros",
+      "Kang",
+      "Kodos",
+      "Predator",
+      "Christopher Johnson",
+    ],
   },
   {
     id: 3,
     time: "FRI, SEP 1, 1939, 1:00 AM DST",
     title: "Extraterrestrial Meetup #-712",
     place: "25°20′42″S 131°02′10″E",
-    attendees: 13,
+    attendees: [
+      "Zoidberg",
+      "E.T.",
+      "Queen Beryl",
+      "Lrrr",
+      "Zaphod Beeblebrox",
+      "Predator",
+      "Indrid Cold",
+    ],
   },
   {
     id: 4,
     time: "WED, NOV 23, 2022, 3:00 AM U",
     title: "Extraterrestrial Meetup #65",
     place: "Slottsskogen, Göteborg",
-    attendees: 2,
+    attendees: [
+      "Zoidberg",
+      "The Doctor",
+      "The Master",
+      "The Thing",
+      "Xenomorph XX121",
+    ],
   },
   {
     id: 5,
     time: "MON, JUNE 10, 1129, 9:00 PM CAST",
     title: "Extraterrestrial Meetup #9",
     place: "45°32′50.8″S 16°49′41.4″W",
-    attendees: 9,
+    attendees: ["Zoidberg", "Optimus Prime", "Spock", "Gordon Shumway"],
   },
 ]
